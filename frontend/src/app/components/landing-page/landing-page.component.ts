cd64bffc3289c5d8185cff1031fd9c9cc7745909
Sieper.fabian@outlook.com <Sieper.fabian@outlook.com>
2025-12-09 00:36:16 +0100

    Implement landing page

--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   imports: [],
-  template: ` <h1>Welcome to NotionQuest!</h1> `,
+  template: `
+    <section class="nes-container is-rounded landing-shell">
+      <h1>Welcome to NotionQuest!</h1>
+
+      <div class="nes-field">
+        <label for="quest-input" class="label-input">Pitch your quest</label>
+        <input
+          id="quest-input"
+          type="text"
+          class="nes-input"
+          placeholder="Enter public Notion Page URL"
+        />
+      </div>
+    </section>
+  `,
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  protected readonly notionUrl = signal<string>('');
+}
